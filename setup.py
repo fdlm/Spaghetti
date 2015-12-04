24ff5171e015d641d8c0c9ed86925874b2d4f4d0
Filip Korzeniowski <filip.korzeniowski@jku.at>
2015-12-04 15:03:01 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Spaghetti',
+    version='0.0.1',
+    packages=['spaghetti'],
+    url='',
+    license='',
+    author='Filip Korzeniowski',
+    author_email='filip.korzeniowski@jku.at',
+    description='', requires=['numpy', 'Lasagne', 'Theano']
+)
