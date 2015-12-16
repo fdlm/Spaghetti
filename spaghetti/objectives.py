Fixed Neg-Loglikelihood computation - new CRF structure

spaghetti/objectives.py
@@ -13,17 +13,13 @@ def neg_log_likelihood(crf, target):
         return lp + c.dot(y_cur.T) + (y_prev.dot(A) * y_cur).sum(axis=1) + \
                (x_cur.dot(W) * y_cur).sum(axis=1)
 
-    init_lp = \
-        y[0].dot(crf.pi) + crf.c.dot(y[0].T) + y[-1].dot(crf.tau) + \
-        (x[0].dot(crf.W) * y[0]).sum(axis=1) - log_z
+    init_lp = y[0].dot(crf.pi) + y[-1].dot(crf.tau) - log_z
 
     seq_lp, _ = theano.scan(
         fn=seq_step,
         outputs_info=init_lp,
         # this starts with y_prev=y[0], y_cur=y[1]
-        sequences=[dict(input=y, taps=[-1, 0]),
-                   # the indexing is necessary so that it starts with x_cur=x[1]
-                   x[1:]],
+        sequences=[dict(input=y, taps=[-1, 0]), x],
         non_sequences=[crf.A, crf.W, crf.c])
 
     # negate log likelihood because we are minimizing